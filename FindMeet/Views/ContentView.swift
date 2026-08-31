18d57a0b7118c7de5e56cecc41970e1320109ccc
Lucas Vieira <prolucascarvalho@gmail.com>
2026-08-31 10:36:03 -0300

    Available traitment for foundation model on device

--- FindMeet/Views/ContentView.swift
+++ FindMeet/Views/ContentView.swift
@@ -7,6 +7,27 @@
 
 import SwiftUI
 import SwiftData
+import FoundationModels
 
 struct ContentView: View {
+    var body: some View {
+        switch SystemLanguageModel.default.availability {
+        case .available:
+            //Puxar view inicial
+            Text("The language model is available.")
+                .padding()
+        case .unavailable(let reason):
+            let text = switch reason {
+            case .appleIntelligenceNotEnabled:
+                "Apple Intelligence is not enabled. Please enable it in Settings."
+            case .deviceNotEligible:
+                "This device is not eligible for Apple Intelligence. Please use a compatible device."
+            case .modelNotReady:
+                "The language model is not ready yet. Please try again later."
+            @unknown default:
+                "The language model is unavailable for an unknown reason."
+            }
+            ContentUnavailableView(text, systemImage: "apple.intelligence.badge.xmark")
+        }
+    }
 }
