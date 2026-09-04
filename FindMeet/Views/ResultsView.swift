d65b44ced709023833fa0becb48834b2dfd6ed33
Lucas Vieira <prolucascarvalho@gmail.com>
2026-09-04 13:24:27 -0300

    Fixing resultView

--- FindMeet/Views/ResultsView.swift
+++ FindMeet/Views/ResultsView.swift
@@ -6,22 +6,44 @@
 //
 
 import SwiftUI
-import Foundation
-import FoundationModels
+
 struct ResultsView: View {
+
+    var flow: MeetFlowState
+
     var body: some View {
-        
-        VStack {
-            Text("Vocês podem...")
-                .font(.largeTitle)
-                .bold()
-                .foregroundColor(.black)
-          
+        if let suggestion = flow.suggestion {
+            InfiniteCarousel(meets: suggestion.suggestions)
+        } else {
+            Text("Nenhuma sugestão disponível.")
         }
     }
 }
 
+// MARK: - Preview
 
-#Preview {
-    ResultsView()
-}
+//#Preview {
+//    let flow = MeetFlowState()
+//    flow.suggestion = Suggestion(suggestions: [
+//        Meet(
+//            title: "Praia no sábado",
+//            description: "Manhã na praia para curtir o sol, o mar e a companhia um do outro.",
+//            time: "Manhã",
+//            ideas: ["Praia", "Bronze", "Sol"]
+//        ),
+//        Meet(
+//            title: "Piquenique no domingo",
+//            description: "Um piquenique à tarde para conversar e dividir lanches",
+//            time: "Tarde",
+//            ideas: ["Lanches", "Natureza", "Toalha"]
+//        ),
+//        Meet(
+//            title: "Cinema a dois",
+//            description: "Um cinema pertinho de casa, com filmes em lançamento, uma comédia romântica",
+//            time: "Noite",
+//            ideas: ["Pipoca", "Casaco", "Escolher juntos"]
+//        )
+//    ])
+//    
+//    return ResultsView(flow: flow)
+//}
