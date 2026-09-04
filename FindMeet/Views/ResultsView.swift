f39d2e0dc97d6784fbab5be0b8951edb9ab7064a
Lucas Vieira <prolucascarvalho@gmail.com>
2026-09-04 12:29:10 -0300

    Apllying Results view

--- FindMeet/Views/ResultsView.swift
+++ FindMeet/Views/ResultsView.swift
@@ -13,13 +13,37 @@ struct ResultsView: View {
 
     var body: some View {
         if let suggestion = flow.suggestion {
-            // renderize a struct Suggestion aqui
-            VStack{
-                InfiniteCarousel(meets: [Meet:
-                                        ])
-            }
+            InfiniteCarousel(meets: suggestion.suggestions)
         } else {
             Text("Nenhuma sugestão disponível.")
         }
     }
 }
+
+// MARK: - Preview
+
+//#Preview {
+//    let flow = MeetFlowState()
+//    flow.suggestion = Suggestion(suggestions: [
+//        Meet(
+//            title: "Praia no sábado",
+//            description: "Manhã na praia para curtir o sol, o mar e a companhia um do outro.",
+//            time: "Manhã",
+//            ideas: ["Praia", "Bronze", "Sol"]
+//        ),
+//        Meet(
+//            title: "Piquenique no domingo",
+//            description: "Um piquenique à tarde para conversar e dividir lanches",
+//            time: "Tarde",
+//            ideas: ["Lanches", "Natureza", "Toalha"]
+//        ),
+//        Meet(
+//            title: "Cinema a dois",
+//            description: "Um cinema pertinho de casa, com filmes em lançamento, uma comédia romântica",
+//            time: "Noite",
+//            ideas: ["Pipoca", "Casaco", "Escolher juntos"]
+//        )
+//    ])
+//    
+//    ResultsView(flow: flow)
+//}
