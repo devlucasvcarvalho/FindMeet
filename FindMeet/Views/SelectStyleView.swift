e3d4b3b601f728c4919a9f92d92f5592264adb74
Cintia Raquel <souza.oliveiracrso@gmail.com>
2026-09-09 09:22:36 -0300

    telas de selectTime e selectStyle corrigidas

--- FindMeet/Views/SelectStyleView.swift
+++ FindMeet/Views/SelectStyleView.swift
@@ -9,71 +9,75 @@ import SwiftUI
 
 struct SelectStyleView: View {
     
-    let backgroundColor: Color = Color(red: 250/255, green: 221/255, blue: 221/255)
     
     @Bindable var flow: MeetFlowState
     @Binding var path: [MeetFlowRoute]
+    @Environment(\.dismiss) var dismiss
     
-    // Estilo que está atualmente selecionado no carrossel
+    let buttonColor: Color = Color(red: 144/255, green: 3/255, blue: 3/255)
+    
     @State private var selectedStyle: MeetStyleEnum = .festive
     
     // String que será armazenada quando o usuário clicar em "Selecionar"
     @State private var selectedStyleString: String = ""
-    
+    private let buttonCircleColor = Color.black.opacity(0.05)
     
     var body: some View {
         ZStack {
-            
-            VStack(spacing: 30) {
-                
-                Text("Qual o estilo do encontro?")
-                    .padding(20)
+            VStack {
+                Spacer()
+                Text("1/2")
                     .font(.title2)
+                    .foregroundStyle(.secondary)
+                
+                
+                Text("Qual atividade tem \n em mente?")
+                    .padding(20)
+                    .font(.title)
                     .bold()
-                // MARK: Carousel
+                    .multilineTextAlignment(.center)
+                
                 
                 InfiniteCarouselView(
                     items: MeetStyleEnum.allCases,
                     selected: $flow.selectedStyle
                 )
+                //                .frame(maxWidth: .infinity, maxHeight: .infinity)
                 
                 
-                // MARK: Current Carousel Selection
-                
-                //            Text("Opção atual: \(selectedStyle.styles)")
-                //                .font(.headline)
                 
                 
-                // MARK: Select Button
-                
-                Button {
+                HStack{
+                    Spacer()
                     
-                    selectedStyleString = selectedStyle.styles
-                    path.append(.selectTime)
-                    
-                } label: {
-                    
-                    Text("Selecionar")
-                        .font(.headline)
-                        .foregroundStyle(.white)
-                        .frame(
-                            maxWidth: .infinity
-                        )
-                        .frame(height: 50)
-                        .background(
-                            Color.blue
-                        )
-                        .clipShape(
-                            RoundedRectangle(
-                                cornerRadius: 15
-                            )
-                        )
+                    Button {
+                        
+                        selectedStyleString = selectedStyle.styles
+                        path.append(.selectTime)
+                        
+                    } label: {
+                        Image(systemName: "arrow.right")
+                            .font(.system(.title3, design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                        //.frame(maxWidth: .infinity)
+                            .padding(/*.vertical,*/ 16)
+                            .background(Color(buttonColor))
+                            .clipShape(Circle())
+                            .shadow(color: Color.white.opacity(0.2),
+                                    radius: 10,
+                                    x: 0, y: 5)
+                    }
                 }
-                .padding(.horizontal, 30)
+                .padding(.horizontal,30)
             }
+        
+               
+            }
+        .toolbar(.hidden, for: .tabBar)
         }
     }
-}
+
 
 
 // MARK: - Preview
--- FindMeet/Views/SelectTimeView.swift
+++ FindMeet/Views/SelectTimeView.swift
@@ -11,35 +11,58 @@ struct SelectTimeView: View {
     
     @Bindable var flow: MeetFlowState
     @Binding var path: [MeetFlowRoute]
+    @Environment(\.dismiss) var dismiss
+    
+    let buttonColor: Color = Color(red: 144/255, green: 3/255, blue: 3/255)
     
     var body: some View {
-        VStack {
-            Text("Qual o horário do encontro?")
-                .font(.title2)
-                .bold()
+        ZStack{
+            VStack {
+                Spacer()
+                Text("2/2")
+                    .font(.title2)
+                    .foregroundStyle(.secondary)
+                
+                Text("Qual o melhor \nhorario?")
+                    .padding(20)
+                    .font(.title)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                
+                
+                
+                InfiniteCarouselView(
+                    items: MeetTimeEnum.allCases,
+                    selected: $flow.selectedTime
+                )
+                
             
-            // MARK: Carousel
-            
-            InfiniteCarouselView(
-                items: MeetTimeEnum.allCases,
-                selected: $flow.selectedTime
-            )
-            
-            // MARK: Select Button
-            
-            Button {
-                path.append(.loading)
-            } label: {
-                Text("Selecionar")
-                    .font(.headline)
-                    .foregroundStyle(.white)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 50)
-                    .background(Color.blue)
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                HStack{
+                    Spacer()
+                    
+                    Button {
+                        
+                        path.append(.loading)
+                        
+                    } label: {
+                        Image(systemName: "arrow.right")
+                            .font(.system(.title3, design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                        //.frame(maxWidth: .infinity)
+                            .padding(/*.vertical,*/ 16)
+                            .background(Color(buttonColor))
+                            .clipShape(Circle())
+                            .shadow(color: Color.white.opacity(0.2),
+                                    radius: 10,
+                                    x: 0, y: 5)
+                    }
+                }
+               
+                .padding(.horizontal, 30)
             }
-            .padding(.horizontal, 30)
         }
+        .toolbar(.hidden, for: .tabBar)
     }
 }
 
